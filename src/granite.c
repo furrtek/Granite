46c72f7b3c67bc46b5148963b1d9c4deaf250905
Furrtek <furrtek@gmail.com>
2015-03-12 01:21:14 +0100

    Color ! Hour ten's digit fix Background gradient

--- src/granite.c
+++ src/granite.c
@@ -7,7 +7,21 @@ Window *window;
 BitmapLayer *digit_layer;
 Layer *bg_layer;
 GBitmap *canvas_bmp;
-GBitmap *numbers_bmp;
+
+#ifdef PBL_COLOR
+	GBitmap *zero_bmp;
+	GBitmap *one_bmp;
+	GBitmap *two_bmp;
+	GBitmap *three_bmp;
+	GBitmap *four_bmp;
+	GBitmap *five_bmp;
+	GBitmap *six_bmp;
+	GBitmap *seven_bmp;
+	GBitmap *eight_bmp;
+	GBitmap *nine_bmp;
+#else
+	GBitmap *numbers_bmp;
+#endif
 
 int32_t icos;
 int16_t angle[4];
@@ -19,7 +33,17 @@ uint8_t rotomask;
 #define REFRESH 40
 
 #ifdef PBL_COLOR
-	uint8_t *numbers_data;
+	uint8_t *zero_data;
+	uint8_t *one_data;
+	uint8_t *two_data;
+	uint8_t *three_data;
+	uint8_t *four_data;
+	uint8_t *five_data;
+	uint8_t *six_data;
+	uint8_t *seven_data;
+	uint8_t *eight_data;
+	uint8_t *nine_data;
+	uint8_t *digit_data;
 	uint8_t *canvas_data;
 #endif
 
@@ -54,12 +78,39 @@ void render_3d_pane(uint8_t d, uint8_t i, uint16_t start, uint16_t stop) {
 				getyy = gety >> 8;
 				#ifdef PBL_COLOR
 					// Get pixel color from 8-bit bitmap
-					cl = ((uint8_t *)numbers_data)[(d * 32) + getxx + (getyy * 320)];
-					// Reduce to 2 values (TODO: Use all colors !)
-					if (cl & 0x3F)
-						cl = 0x80;
-					else
-						cl = 0;
+					switch (d) {
+						case 0:
+							digit_data = zero_data;
+							break;
+						case 1:
+							digit_data = one_data;
+							break;
+						case 2:
+							digit_data = two_data;
+							break;
+						case 3:
+							digit_data = three_data;
+							break;
+						case 4:
+							digit_data = four_data;
+							break;
+						case 5:
+							digit_data = five_data;
+							break;
+						case 6:
+							digit_data = six_data;
+							break;
+						case 7:
+							digit_data = seven_data;
+							break;
+						case 8:
+							digit_data = eight_data;
+							break;
+						case 9:
+							digit_data = nine_data;
+							break;
+					}
+					cl = ((uint8_t *)digit_data)[getxx + (getyy * 32)];
 				#else
 					// Get pixel color from 1-bit bitmap
 					cl = ((uint8_t *)numbers_bmp->addr)[(d * 4) + (getxx >> 3) + (getyy * 40)];
@@ -71,15 +122,13 @@ void render_3d_pane(uint8_t d, uint8_t i, uint16_t start, uint16_t stop) {
 				puty = ya + y;
 				// The only difference for plotting here is the data access pointer:
 				#ifdef PBL_COLOR
+					((uint8_t*)canvas_data)[putx + (puty * 160)] = cl;
+				#else
 					// Get previous 8 pixels (1 byte)
-					ci = ((uint8_t*)canvas_data)[(putx >> 3) + (puty * 20)];
+					ci = ((uint8_t*)canvas_bmp->addr)[(putx >> 3) + (puty * 20)];
 					// Merge with new pixel
 					ci |= (cl >> (7 - (putx & 7)));
 					// Put back
-					((uint8_t*)canvas_data)[( putx>>3 ) + (puty * 20)] = ci;
-				#else
-					ci = ((uint8_t*)canvas_bmp->addr)[(putx >> 3) + (puty * 20)];
-					ci |= (cl >> (7 - (putx & 7)));
 					((uint8_t*)canvas_bmp->addr)[(putx >> 3) + (puty * 20)] = ci;
 				#endif
 				getx = getx + getxd;	// Next pixel
@@ -95,15 +144,15 @@ void render() {
 	
 	// First, clear canvas with black
 	#ifdef PBL_COLOR
-		memset(canvas_data,0,160 * 90 / 8);
+		memset(canvas_data, 0, 160 * 90);
 	#else
-		memset(canvas_bmp->addr,0,160 * 90 / 8);
+		memset(canvas_bmp->addr, 0, 160 * 90 / 8);
 	#endif
 	
 	// For each of the 4 digits, render top and bottom pane
 	for (d = 0; d < 4; d++) {
-		render_3d_pane(digit_top[d],d,(0x7FFF * 3 / 4),(0x7FFF * 5 / 4));
-		render_3d_pane(digit_bottom[d],d,(0x7FFF * 5 / 4),(0x7FFF * 7 / 4));
+		render_3d_pane(digit_top[d], d, (0x7FFF * 3 / 4), (0x7FFF * 5 / 4));
+		render_3d_pane(digit_bottom[d], d, (0x7FFF * 5 / 4), (0x7FFF * 7 / 4));
 	}
 	
 	// Kindly ask Pebble OS to refresh the layer
@@ -161,7 +210,6 @@ void roto(void *data) {
 }
 
 void handle_timechanges(struct tm *tick_time, TimeUnits units_changed) {
-	uint8_t d;
 	uint8_t sec_ten, min_unit, min_ten, hour_unit, hour_ten;
 	bool needrender;
 
@@ -194,14 +242,14 @@ void handle_timechanges(struct tm *tick_time, TimeUnits units_changed) {
 			if (angle[0] > 0x3FFF) {
 				angle[0] -= 0x4000;
 				// Swap panes
-				digit_top[0] = (hour_ten + 1) % 6;	
+				digit_top[0] = (hour_ten + 1) % 3;	
 				digit_bottom[0] = hour_ten;
 			} else {
 				digit_top[0] = hour_ten;
 				if (hour_ten - 1 >= 0)
-					digit_bottom[0] = (hour_ten - 1) % 6;
+					digit_bottom[0] = (hour_ten - 1) % 3;
 				else
-					digit_bottom[0] = 5;
+					digit_bottom[0] = 2;
 			}
 			
 			if (min_ten == 0) {
@@ -278,10 +326,33 @@ static void tap_handler(AccelAxisType axis, int32_t direction) {
 }
 	
 void bg_fill(Layer *me, GContext* ctx) {
-	// Fill BG layer with black
+	uint32_t isin = 0;
+	int32_t acc = 0;
+	uint8_t y;
+	
 	graphics_context_set_fill_color(ctx,GColorBlack);
 	graphics_context_set_stroke_color(ctx,GColorBlack);
+	
 	graphics_fill_rect(ctx, GRect(0, 0, 144, 168), 0, 0);
+	
+	#ifdef PBL_COLOR
+		graphics_context_set_stroke_color(ctx,(GColor8){0b11010101});
+	#else
+		graphics_context_set_stroke_color(ctx,GColorWhite);
+	#endif
+	
+	// Fill BG layer with some sort of procedural gradient
+
+    for (y = 0; y < 84; y++) {
+        acc += sin_lookup(isin);
+        if (acc > 0x7FFF) {
+            acc -= 0x10000;
+        } else {
+            graphics_draw_line(ctx,GPoint(0, y),GPoint(144, y));
+            graphics_draw_line(ctx,GPoint(0, 168-y),GPoint(144, 168-y));
+        }
+        isin += (0x8000 / 100);
+    }
 }
  
 void handle_init(void) {
@@ -297,7 +368,8 @@ void handle_init(void) {
 	// Rendering layer, a bit wider than screen (4 pixels out left and right)
 	digit_layer = bitmap_layer_create(GRect(-4, bounds.origin.y, 152, 168));
 	#ifdef PBL_COLOR
-		canvas_bmp = gbitmap_create_blank(GSize(160, 90),GBitmapFormat1Bit);
+		canvas_bmp = gbitmap_create_blank(GSize(160, 90),GBitmapFormat8Bit);
+		bitmap_layer_set_compositing_mode(digit_layer, GCompOpSet);
 	#else
 		canvas_bmp = gbitmap_create_blank(GSize(160, 90));
 	#endif
@@ -305,13 +377,58 @@ void handle_init(void) {
 	layer_add_child(window_layer, bitmap_layer_get_layer(digit_layer));
 	
 	// Load numbers "font", drawn with love in MSPaint
-	numbers_bmp = gbitmap_create_with_resource(RESOURCE_ID_NUMBERS);
+	#ifdef PBL_COLOR
+		zero_bmp = gbitmap_create_with_resource(RESOURCE_ID_ZERO);
+		one_bmp = gbitmap_create_with_resource(RESOURCE_ID_ONE);
+		two_bmp = gbitmap_create_with_resource(RESOURCE_ID_TWO);
+		three_bmp = gbitmap_create_with_resource(RESOURCE_ID_THREE);
+		four_bmp = gbitmap_create_with_resource(RESOURCE_ID_FOUR);
+		five_bmp = gbitmap_create_with_resource(RESOURCE_ID_FIVE);
+		six_bmp = gbitmap_create_with_resource(RESOURCE_ID_SIX);
+		seven_bmp = gbitmap_create_with_resource(RESOURCE_ID_SEVEN);
+		eight_bmp = gbitmap_create_with_resource(RESOURCE_ID_EIGHT);
+		nine_bmp = gbitmap_create_with_resource(RESOURCE_ID_NINE);
+	#else
+		numbers_bmp = gbitmap_create_with_resource(RESOURCE_ID_NUMBERS);
+	#endif
 	
 	// Get pointers to raw data of the bitmap and canvas
 	#ifdef PBL_COLOR
-		numbers_data = gbitmap_get_data(numbers_bmp);
+		zero_data = gbitmap_get_data(zero_bmp);
+		one_data = gbitmap_get_data(one_bmp);
+		two_data = gbitmap_get_data(two_bmp);
+		three_data = gbitmap_get_data(three_bmp);
+		four_data = gbitmap_get_data(four_bmp);
+		five_data = gbitmap_get_data(five_bmp);
+		six_data = gbitmap_get_data(six_bmp);
+		seven_data = gbitmap_get_data(seven_bmp);
+		eight_data = gbitmap_get_data(eight_bmp);
+		nine_data = gbitmap_get_data(nine_bmp);
 		canvas_data = gbitmap_get_data(canvas_bmp);
 	#endif
+	
+	/*#ifdef PBL_COLOR
+	text_layersg = text_layer_create((GRect){{32, 72}, {bounds.size.w-32-32, 20}});
+	switch (gbitmap_get_format(numbers_bmp)) {
+		case GBitmapFormat1Bit:
+			text_layer_set_text(text_layersg, "1BIT");
+			break;
+		case GBitmapFormat8Bit:
+			text_layer_set_text(text_layersg, "8BIT");
+			break;
+		case GBitmapFormat1BitPalette:
+			text_layer_set_text(text_layersg, "1BP");
+			break;
+		case GBitmapFormat2BitPalette:
+			text_layer_set_text(text_layersg, "2BP");
+			break;
+		case GBitmapFormat4BitPalette:
+			text_layer_set_text(text_layersg, "4BP");
+			break;
+	}	
+	text_layer_set_text_alignment(text_layersg, GTextAlignmentCenter);
+	layer_add_child(window_layer, text_layer_get_layer(text_layersg));
+#endif*/
 
 	// Get notified of taps
 	accel_tap_service_subscribe(tap_handler);
